7f42bd5554d2dcb1027cdef30eb24cdf46ec2a76
Wyr' Villazorda <wyrlvillazorda@gmail.com>
2023-06-26 10:31:57 +0800

    database

--- app/src/main/java/com/example/movieapp/data/database/MovieDatabase.java
+++ app/src/main/java/com/example/movieapp/data/database/MovieDatabase.java
@@ -1,6 +1,32 @@
 package com.example.movieapp.data.database;
 
+import android.content.Context;
+
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public abstract class MovieDatabase extends RoomDatabase {
+    public abstract MovieDatabase wordDao();
+
+    private static volatile MovieDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static MovieDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (MovieDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    MovieDatabase.class, "movie_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
 }
